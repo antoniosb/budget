ec75001922f1b9010a45c34e900a338921edb91d
Antonio Augusto <antonioabritto@gmail.com>
2017-03-06 00:25:38 -0300

    defining a Budget.list_transactions function

--- lib/budget.ex
+++ lib/budget.ex
@@ -1,18 +1,9 @@
 defmodule Budget do
-  @moduledoc """
-  Documentation for Budget.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Budget.hello
-      :world
-
-  """
   def hello do
     :world
+  end
+
+  def list_transactions do
+    "listing transactions"
   end
 end
