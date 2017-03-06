28d8ae0a43398e7864ff50ead9ecde59a027469f
Antonio Augusto <antonioabritto@gmail.com>
2017-03-06 00:29:46 -0300

    add a shortdoc to our task so it appear on "mix help". (dont forget to "mix compile" before trying it out)

--- lib/mix/tasks/list_transactions.ex
+++ lib/mix/tasks/list_transactions.ex
@@ -1,6 +1,7 @@
 defmodule Mix.Tasks.ListTransactions do
   use Mix.Task
-
+  
+  @shortdoc "List transactions from a specific CSV file"
   def run(_) do
     Budget.list_transactions |> IO.inspect
   end
